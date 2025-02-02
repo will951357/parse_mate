dcb010dcd81be668eaef12bb16c8ad1fa16ed48a
William <williamsilva14@hotmail.com>
2025-02-01 21:53:53 -0300

    Adiconando o método filter_by_size

--- parse_mate/file_finder.py
+++ parse_mate/file_finder.py
@@ -5,6 +5,7 @@ Módulo focado em buscar arquivos nos diretórios selecionados
 
 import os
 import fnmatch
+import pdb
 
 from datetime import date, datetime
 from enum import Enum
@@ -65,23 +66,34 @@ class FileFinder:
         if not os.path.isdir(self.directory):
             raise NotADirectoryError(f"{self.directory.__str__} não é um diretório válido")
         
-    def find_files(self) -> List[Union[str, Path]]:
+    def find_files(self, max_depth: Optional[int] = None) -> List[Union[Path]]:
         """
         Econtra os arquivos desejados no diretório de busca
+
+        Args:
+            max_depth: Determina até qual profundidade de subpastas será realizada a busca.
+
+        Raises: 
+            FileExistsError: Quando algum arquivo da lista não existe.
 
         Returns:
             Uma lista com os arquivos encontrados
         """
-
         matched_files = []
+        base_depth = self.directory.resolve().parts
 
-        for root, _, files in os.walk(self.directory):
+        for root, dirs, files in os.walk(self.directory):
+            current_depth = len(Path(root).resolve().parts) - len(base_depth)
+
+            if max_depth is not None and current_depth >= max_depth:
+                del dirs[:]
+
             for file in files:
                 if fnmatch.fnmatch(file, self.filename_pattern):
-                    matched_files.append(os.path.join(root, file))
+                    matched_files.append(Path(os.path.join(root, file)))
         return matched_files
     
-    def filter_by_date(
+    def filter_by_date(self,
             list_of_files: List[Union[str, Path]],
             filter_date: Union[date, datetime], 
             filter_type: FilterType = FilterType.EQUAL
@@ -99,16 +111,53 @@ class FileFinder:
         """
         filtered_files = []
 
+        self._verify_existence(list_of_files)
+
         for file_path in list_of_files:
             file_path_str = str(file_path) if isinstance(file_path, Path) else file_path
+
             file_mod_time = datetime.fromtimestamp(os.path.getmtime(file_path_str))
+            if isinstance(filter_date, date):
+                file_mod_time = file_mod_time.date()
 
             if _FILTER_OPERATORS[filter_type](file_mod_time, filter_date):
-                filtered_files.append(file_path)
+                filtered_files.append(Path(file_path))
 
         return filtered_files
 
-    def order_files(
+    def filter_by_size(self, 
+                   list_of_files: List[Union[str, Path]], 
+                   min_size: Optional[int] = None, 
+                   max_size: Optional[int] = None) -> List[Union[str, Path]]:
+        """
+        Realiza uma filtragem pelo tamanho do arquivo
+
+        Args:
+            List_of_files: lista de arquivos a serem filtrados.
+            min_size: Tamanho mínimo
+            max_size: Tamanho máximo
+
+        Raises:
+            FileExistsError: Caso algum arquivo não exista
+
+        Returns:
+            Lista de arquivos filtrados.
+        """
+
+        filtered_files = []
+
+        self._verify_existence(list_of_files)
+
+        for file_path in list_of_files:
+            file_path = Path(file_path) if isinstance(file_path, str) else file_path
+            file_size = file_path.stat().st_size
+
+            if (min_size is None or file_size >= min_size) and (max_size is None or file_size <= max_size):
+                filtered_files.append(file_path)
+
+        return filtered_files
+    
+    def order_files(self,
         list_of_files: List[Union[str, Path]],
         order_by: OrderBy = OrderBy.MODIFIED_DESC
     ) -> List[str]:
@@ -128,18 +177,17 @@ class FileFinder:
         if not list_of_files:
             return []
         
-        if not isinstance(list_of_files[0], str) or not isinstance(list_of_files[0], Path):
-            raise ValueError(f"Tipo de dados {type(list_of_files[0])} não compátivel")
-        
         list_of_files = [Path(file) if isinstance(file, str) else file for file in list_of_files]
 
-        for file in list_of_files:
-            if not file.exists:
-                raise FileExistsError(f"O arquivo {file.__str__} não existe")
+        self._verify_existence(list_of_files)
         
         return _ORDER_OPERATIONS.get(order_by, _ORDER_OPERATIONS[OrderBy.NAME_ASC])(list_of_files)
 
+    @staticmethod
     def _verify_existence(list_of_files: List[Path]) -> bool:
         """
-        Verifica
+        Verifica a existencia do arquivo
         """
+        for file in list_of_files:
+            if not file.exists:
+                raise FileExistsError(f"O arquivo {file.__str__} não existe")
